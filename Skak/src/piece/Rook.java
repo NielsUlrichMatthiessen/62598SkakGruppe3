45bb2e2c65801dffcae1061134d556029504de79
Ntayi <38472039+Ntayi@users.noreply.github.com>
2018-04-18 11:12:01 +0200

    Update Rook.java

--- Skak/src/piece/Rook.java
+++ Skak/src/piece/Rook.java
@@ -1,21 +1,44 @@
 package piece;
 
 import java.util.ArrayList;
+import java.awt.Point;
 
 public class Rook extends Piece {
+	
 	private static int value = 5;
+	Collection<Point> legalMoves;
 	
-	
-	
-	
-	
-	
-	
-	
-	public Rook() {
-		super(Type.Rook, value);
+	public Rook(Color color) {
+		
+		super(Type.Rook, color, value);
+		legalMoves = new ArrayList<>();
 
 	}
-
+	
+	 public Collection<Point> getLegalMoves() {
+		 //mangler rokade
+		 
+		 //op
+		 for (int i = 1; i < 8; i++) {
+			 legalMoves.add(new Point (+i, 0));
+		 }
+		 
+		 //ned
+		 for (int i = 1; i > 8; i++) {
+			 legalMoves.add(new Point(-i, 0));
+		 }
+		 
+		 //højre
+		 for (int i = 1; i < 8; i++) {
+			 legalMoves.add(new Point(0,+i));
+		 }
+		 
+		 //venstre
+		 for (int i = 1; i > 8; i++) {
+			 legalMoves.add(new Point(0,-i));
+		 }
+		 
+		 return legalMoves;
+	 }
 
 }
