e72c151c6d75e76fc44e463408a966cb0aec8209
Ntayi <38472039+Ntayi@users.noreply.github.com>
2018-04-18 11:04:19 +0200

    Update Knight.java

--- Skak/src/piece/Knight.java
+++ Skak/src/piece/Knight.java
@@ -1,17 +1,33 @@
 package piece;
 
 import java.util.ArrayList;
+import java.awt.Point;
 
 public class Knight extends Piece {
+	
 	private static int value = 3;
+	Collection<Point> legalMoves;
 	
 	
 	
-	public Knight() {
-		super(Type.Knight, value);
+	public Knight(Color color) {
+		super(Type.Knight, color, value);
+		legalMoves = new ArrayList<>();
 		// TODO Auto-generated constructor stub
 	}
-
 	
+	 public Collection<Point> getLegalMoves() {
+		 
+		 legalMoves.add(new Point(-2, +1));
+		 legalMoves.add(new Point(-1, +2));
+		 legalMoves.add(new Point(+1, +2));
+		 legalMoves.add(new Point(+2, +1));
+		 legalMoves.add(new Point(+2, -1));
+		 legalMoves.add(new Point(+1, -2));
+		 legalMoves.add(new Point(-1, -2));
+		 legalMoves.add(new Point(-2, -1));
+		 
+		 return legalMoves;
+	 }
 
 }
